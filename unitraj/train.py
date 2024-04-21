862229cc183eaad3a4de922ff4a9a3adc1c7905c
Alan <fenglan18@outlook.com>
2024-04-21 07:03:50 +0200

    fix bug

--- unitraj/train.py
+++ unitraj/train.py
@@ -48,7 +48,7 @@ def train(cfg):
 
     trainer = pl.Trainer(
         max_epochs=cfg.method.max_epochs,
-        logger=None if cfg.debug else WandbLogger(project="unitraj", name=cfg.exp_name),
+        logger=None if cfg.debug else WandbLogger(project="unitraj", name=cfg.exp_name, id=cfg.exp_name),
         devices=1 if cfg.debug else cfg.devices,
         gradient_clip_val=cfg.method.grad_clip_norm,
         accelerator="cpu" if cfg.debug else "gpu",
